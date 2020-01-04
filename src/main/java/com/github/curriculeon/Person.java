e75e70e0464a542951992a2505482c999bf6d791
Antonio Perdigoto <antonio@globalandsocial.com>
2020-01-04 14:58:21 -0500

    Uploading files

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,25 +1,35 @@
 package com.github.curriculeon;
 
 /**
- * Created by leon on 12/16/2019.
+ * Created by Antonio Perdigoto on 12/16/2019.
  */
 public class Person {
-    public Person(){}
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+        this.firstName = "";
+        this.lastName = "";
+    }
 
     public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getFirstName() {
-        return null;
+        return firstName;
     }
 
     public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
-        return null;
+        return lastName;
     }
 
     public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
